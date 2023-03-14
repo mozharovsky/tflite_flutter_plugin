4c571a49a8364b78273fe27f3a90e45cbaeaa571
Eugene Mozharovsky <mozharovsky@live.com>
2023-03-14 12:30:45 +0300

    feat: export coreml delegate

--- lib/tflite_flutter.dart
+++ lib/tflite_flutter.dart
@@ -11,6 +11,7 @@ import 'src/bindings/bindings.dart';
 
 export 'src/bindings/types.dart';
 export 'src/delegate.dart';
+export 'src/delegates/coreml_delegate.dart';
 export 'src/delegates/gpu_delegate.dart';
 export 'src/delegates/metal_delegate.dart';
 export 'src/delegates/xnnpack_delegate.dart';
@@ -18,8 +19,8 @@ export 'src/interpreter.dart';
 export 'src/interpreter_options.dart';
 export 'src/quanitzation_params.dart';
 export 'src/tensor.dart';
-export 'src/util/list_shape_extension.dart';
 export 'src/util/byte_conversion_utils.dart';
+export 'src/util/list_shape_extension.dart';
 
 /// tflite version information.
 String get version => tfLiteVersion().toDartString();
